26f906c6f440692e7d2fe3392ebbf78ecf29360d
Abdu201213 <sabdu9049@gamil.com>
2024-12-24 00:33:09 +0300

    setting up the sign up route and controller

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,13 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import db from './configures/db.configure.js'
+import userRoute from './routes/user.route.js'
 
 dotenv.config();
 const port = process.env.PORT || 3000;
 const app = express();
-
+app.use(express.json())
+app.use('/user',userRoute);
 app.listen(port,()=>{
    console.log(`server is listening on port:${port}`)
    db()
--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js"
-const getAllUser = async ()=>{
+import bcrypt from 'bcrypt'
+const getAllUser = async (req,res)=>{
    try{
-      const user = User.find();
+      const user = await User.find();
       if(user.length === 0){
          return res.status(200).json({
             message:"No users are found in the database.",
@@ -13,12 +14,33 @@ const getAllUser = async ()=>{
          user
       });
    }catch(err){
-      console.log(err.message)
+      console.log(err)
       res.status(500).json({
          message:"Error while communicating with database.",
-         error:errr.message
+         error:err.message
       })
    }
 }
 
 export default getAllUser;
+
+export const signup = async (req,res)=>{
+   try{
+      const {name, email, password} = req.body;
+      console.log(req.body)
+      if(!email || !password || !name || password.length < 6){
+         return res.status(422).json({message:"Invalid data"});
+      }
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+      const user = new User({name, email,password:hashedPassword});
+      await user.save()
+      return res.status(201).json({message:"User created successfully."});
+   }catch(err){
+      console.log(err.message)
+      return res.status(500).json({
+         message:"can't insertt data while signup",
+         error:err.message
+      });
+   }
+}
