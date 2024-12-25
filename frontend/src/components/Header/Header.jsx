5b454b1d805bd67d9d324e6129be5d0e28ee0d51
Abdu201213 <sabdu9049@gamil.com>
2024-12-25 22:31:05 +0300

    configure the header

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {AppBar, Toolbar} from '@mui/material'
+function Header() {
+  return (
+    <div>
+      <AppBar>
+         <Toolbar></Toolbar>
+      </AppBar>
+    </div>
+  )
+}
+
+export default Header
