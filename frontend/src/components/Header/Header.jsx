2e6dee6fe9cbc624767da757b199bb63dbf4a389
Abdu201213 <sabdu9049@gamil.com>
2024-12-26 10:45:55 +0300

    adjustin the position sticky

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,17 +1,20 @@
 import React,{useState} from 'react'
+import { Link } from 'react-router-dom';
 import {AppBar, Toolbar, Tabs,Tab} from '@mui/material'
 import TravelExploreIcon from "@mui/icons-material/TravelExplore";
 function Header() {
-   const linkArr = ["home", "Diaries", "Auth"];
+   const linkArr = ["home", "diaries", "auth"];
    const [value, setValue] = useState();
   return (
     <div>
-      <AppBar sx={{bgcolor:"transparent"}}>
+      <AppBar sx={{bgcolor:"transparent", position:"sticky"}}>
          <Toolbar>
              <TravelExploreIcon sx={{color:"gray"}} />
              <Tabs value={value} onChange={(e,val)=>setValue(val)} sx ={{ml:"auto",textDecoration:"none",}}>
              {linkArr.map((link)=>(
-               <Tab key ={link} label ={link}  sx={{textDecoration:"none",":hover":{
+               <Tab key ={link} 
+               LinkComponent={Link} to ={`/${link === "home" ? "" : link}`}
+               label ={link}  sx={{textDecoration:"none",":hover":{
                   textDecoration:"underline",
                   textUnderlineOffset:"18px"}
                }} />
