02d6d42016f9046ec8134ae8333d54a6c577af62
Abdu201213 <sabdu9049@gamil.com>
2024-12-27 21:49:05 +0300

    sending user data

--- frontend/src/ApiHelper/Helpers.jsx
+++ frontend/src/ApiHelper/Helpers.jsx
@@ -12,3 +12,16 @@ const Helpers = async ()=>{
 }
 
 export default Helpers;
+
+const sendAuthData = async (signup, data) => {
+   const response = await axios.post(`http://localhost:3000/${signup ? "signup" : "login"}`, data)
+   .catch((err)=>{
+      console.log(err);
+   });
+   if(response.status !== 200 || response.status !== 201){
+      console.log("Error fetching data");
+      return null;
+   }
+   console.log(response.data);
+   return response.data;
+}
