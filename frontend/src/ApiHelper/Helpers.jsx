27e886aa9760e9880b6b043c158dcbecff5ea79f
Abdu201213 <sabdu9049@gamil.com>
2024-12-29 11:54:25 +0300

    modify the response .status condition

--- frontend/src/ApiHelper/Helpers.jsx
+++ frontend/src/ApiHelper/Helpers.jsx
@@ -15,11 +15,9 @@ export default Helpers;
 export const sendAuthData = async (signup, data) => {
    try{
    const response = await axios.post(`http://localhost:3000/user/${signup ? "signup" : "login"}`, data);
-   if(response.status !== 200 && response.status !== 201){
-      console.log("Error fetching data");
-      return response.data;
+   if(response.status > 300){
+      return null;
    }
-   console.log(response.data);
    return response.data;
    }
    catch(err){
